b520e210634cc3897b02bdd10e5b621d41fda6c1
krushnaraj388 <78199182+krushnaraj388@users.noreply.github.com>
2021-08-13 14:33:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+noseX=0
+noseY=0
+difference=0;
+rightWristX=0;
+leftWristX=0;
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas=createCanvas(550,550);
+    canvas.position(560,150);
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Initialized');
+}
+
+function draw(){
+    background('#969A97');
+
+    document.getElementById("square_sides").innerHTML="Width and Height of a square will be="+difference+"px";
+    fill('#F90093');
+    stroke('#F90093');
+    square(noseY,noseX,difference);
+}
+function gotPoses(results){
+    if(results.length>0)
+    {
+        console.log(results);
+        noseY=results[0].pose.nose.y;
+        noseX=results[0].pose.nose.x;
+        console.log("noseX="+noseX+"noseY"+noseY);
+
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+        difference=floor(leftWristX-rightWristX);
+
+        console.log("leftWristX="+ leftWristX+"rightWristX="+ rightWristX+"difference="+difference);
+    }
+}
